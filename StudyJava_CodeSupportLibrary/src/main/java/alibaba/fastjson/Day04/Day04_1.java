778e862a34cabca8641a13979a46df16e81adcd8
MXuDong <2941884109@qq.com>
2019-02-04 18:33:14 +0800

    Day04完成

--- StudyJava_CodeSupportLibrary/src/main/java/alibaba/fastjson/Day04/Day04_1.java
+++ StudyJava_CodeSupportLibrary/src/main/java/alibaba/fastjson/Day04/Day04_1.java
@@ -1,5 +1,8 @@
 package alibaba.fastjson.Day04;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
 import java.io.InputStream;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
@@ -18,17 +21,25 @@ FastJson - JSON 类
  */
 public class Day04_1 {
     public static void main(String[] args) {
-        InputStream imputStream = AccessController.doPrivileged(new PrivilegedAction<InputStream>() {
-            public InputStream run() {
-                ClassLoader cl = Thread.currentThread().getContextClassLoader();
-                if (cl != null) {
-                    return cl.getResourceAsStream("fastjson.properties");
-                } else {
-                    return ClassLoader.getSystemResourceAsStream("fastjson.properties");
-                }
-            }
-        });
+        People people = new People();
+        people.peopleName = "测试";
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("测试默认用例", "测试默认");
+        jsonObject.put("people",people);
+        jsonObject.put("people2", people);
 
-        System.out.println(imputStream);
+        JSONArray jsonArray = new JSONArray();
+        jsonArray.add(new JSONObject());
+        jsonArray.add(jsonObject);
+        jsonArray.add(people);
+
+        System.out.println(jsonObject);
+        System.out.println(jsonArray);
+
+    }
+
+    static class People{
+        public String peopleName;
     }
 }
+
