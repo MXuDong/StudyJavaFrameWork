10f9e6b391ee0c35bf3d6333895a19ecf46b7150
MXuDong <2941884109@qq.com>
2019-02-06 21:46:28 +0800

    通过Serialize序列化

---
+++ StudyJava_CodeSupportLibrary/src/main/java/alibaba/fastjson/Day05/innerPackage/PeopleBySerialize.java
@@ -0,0 +1,13 @@
+package alibaba.fastjson.Day05.innerPackage;
+
+/**
+ * @author Dong
+ * <p>
+ * Class Name : PeopleBySerialize
+ * Create Time : 21:46
+ * Create Date : 2019/2/6
+ * Project : StudyJavaFrameWork
+ */
+
+public class PeopleBySerialize implements {
+}
