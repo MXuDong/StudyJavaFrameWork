745c354b0afa2087e98c8df78d7d29a4b7482db3
MXuDong <2941884109@qq.com>
2019-02-07 13:55:11 +0800

    学习序列化

---
+++ StudyJava_CodeSupportLibrary/src/main/java/alibaba/fastjson/Day05/Day05_1.java
@@ -0,0 +1,63 @@
+package alibaba.fastjson.Day05;
+
+import alibaba.fastjson.Day05.innerPackage.PeopleBySerialize;
+
+import java.io.*;
+
+/**
+ * @author Dong
+ * <p>
+ * Class Name : Day05_1
+ * Create Time : 21:38
+ * Create Date : 2019/2/6
+ * Project : StudyJavaFrameWork
+ */
+
+public class Day05_1 {
+    public static void main(String[] args) {
+        PeopleBySerialize people = new PeopleBySerialize();
+        people.setUserAge(15);
+        people.setUserName("赵本山");
+        people.setUserNumber(151);
+        System.out.println(people);
+
+        ObjectOutputStream oos = null;
+
+        try {
+            oos = new ObjectOutputStream(new FileOutputStream("testSerialize"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                oos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        FileInputStream fis = null;
+        ObjectInputStream ois = null;
+        try {
+            fis = new FileInputStream("testSerialize");
+            ois = new ObjectInputStream(fis);
+            PeopleBySerialize people1 = (PeopleBySerialize)ois.readObject();
+            System.out.println(people1);
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            try {
+                fis.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                ois.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+}
--- StudyJava_CodeSupportLibrary/src/main/java/alibaba/fastjson/Day05/innerPackage/PeopleBySerialize.java
+++ StudyJava_CodeSupportLibrary/src/main/java/alibaba/fastjson/Day05/innerPackage/PeopleBySerialize.java
@@ -1,5 +1,7 @@
 package alibaba.fastjson.Day05.innerPackage;
 
+import java.io.Serializable;
+
 /**
  * @author Dong
  * <p>
@@ -9,5 +11,40 @@ package alibaba.fastjson.Day05.innerPackage;
  * Project : StudyJavaFrameWork
  */
 
-public class PeopleBySerialize implements {
+public class PeopleBySerialize implements Serializable {
+    private String userName;
+    private int userAge;
+    private int userNumber;
+
+    public int getUserNumber() {
+        return userNumber;
+    }
+
+    public void setUserNumber(int userNumber) {
+        this.userNumber = userNumber;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(int userAge) {
+        this.userAge = userAge;
+    }
+
+    @Override
+    public String toString() {
+        return "PeopleBySerialize{" +
+                "userName='" + userName + '\'' +
+                ", userAge=" + userAge +
+                '}';
+    }
 }
