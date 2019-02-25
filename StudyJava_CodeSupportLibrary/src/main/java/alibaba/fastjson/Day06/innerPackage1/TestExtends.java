ec850fb22973ce3d7f56c22e51f4cd13f2d9c1a9
MXuDong <2941884109@qq.com>
2019-02-25 15:11:12 +0800

    添加主类

---
+++ StudyJava_CodeSupportLibrary/src/main/java/alibaba/fastjson/Day06/innerPackage1/TestExtends.java
@@ -0,0 +1,14 @@
+package alibaba.fastjson.Day06.innerPackage1;
+
+/**
+ * @author Dong
+ * <p>
+ * Class Name : TestExtends
+ * Create Time : 15:10
+ * Create Date : 2019/2/25
+ * Project : StudyJavaFrameWork
+ */
+
+public class TestExtends {
+
+}
