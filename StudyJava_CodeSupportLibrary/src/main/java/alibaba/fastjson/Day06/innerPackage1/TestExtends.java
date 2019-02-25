da65631131b5c5213b0719474ac526e09ccb548a
MXuDong <2941884109@qq.com>
2019-02-25 15:18:36 +0800

    完成测试1

--- StudyJava_CodeSupportLibrary/src/main/java/alibaba/fastjson/Day06/innerPackage1/TestExtends.java
+++ StudyJava_CodeSupportLibrary/src/main/java/alibaba/fastjson/Day06/innerPackage1/TestExtends.java
@@ -1,5 +1,7 @@
 package alibaba.fastjson.Day06.innerPackage1;
 
+import alibaba.fastjson.Day06.SerializableUtil.SU;
+
 /**
  * @author Dong
  * <p>
@@ -10,5 +12,15 @@ package alibaba.fastjson.Day06.innerPackage1;
  */
 
 public class TestExtends {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setStudentNumber("11011");
+        student.setPeopleAge(15);
+        student.setPeopleName("赵本山");
 
+        System.out.println(student);
+        SU.write("testSerializable", student);
+        Student beRead = (Student) SU.read("testSerializable");
+        System.out.println(beRead);
+    }
 }
