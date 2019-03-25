4df24b8b58ee80649d7d375c9fa10b6614d7de59
MXuDong <2941884109@qq.com>
2019-03-25 08:10:30 +0800

    测试OOM异常发生的过程

---
+++ JVMBase/src/Day01/HeapOOM.java
@@ -0,0 +1,18 @@
+package Day01;
+
+/**
+ * @author Dong
+ * <p>
+ * Class Name : HeapOOM
+ * Create Time : 8:08
+ * Create Date : 2019/3/25
+ * Project : StudyJavaFrameWork
+ *
+ * JVM Params: -Xms20m -Xmx20m -XX:HeapDumpOnOutOfMemoryError
+ */
+
+public class HeapOOM {
+    static class OOMObject{
+
+    }
+}
